0b1df94edf9ccb730fd09435854d2be166705420
orxshi <orxan.shibli@gmail.com>
2023-03-28 23:31:43 +0300

    >

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ const double TL = 300;
 const double TH = 600;
 const double VL = 0.1;
 const double cr = 6; // compression ratio
+const double VH = cr * VL;
 const double m = 1;
         
 double P[n];
@@ -19,7 +20,6 @@ double V[n];
 double T[n];
 
 int nquad = n / 4;
-double VH = 0.1 * cr;
 
 double dw(int j)
 {
@@ -31,15 +31,55 @@ double dw(int j)
     return P[j] * (V[j] - V[j-1]);
 }
 
+double dq(int j)
+{
+    if (j == 0)
+    {
+        return 0.;
+    }
+
+    return T[j] * (S[j] - S[j-1]);
+}
+
 double work = 0.;
 
-void isothermal_heat_rejection(int k, double VI)
+void isobaric_heat_rejection(int k, double PC)
 {
-    double inc = (VH - VI) / (nquad+1);
+    double inc = (TH - TL) / (nquad+1);
     for (int i=0; i<nquad; ++i)
     {
         int j = i + k * nquad;
-        V[j] = VH - i * inc;
+        T[j] = TH - i * inc;
+        P[j] = PC;
+        V[j] = m * R * T[j] / P[j];
+
+        work += dw(j);
+        heat += 
+    }
+}
+
+void isobaric_heat_addition(int k, double PC)
+{
+    double inc = (TH - TL) / (nquad+1);
+    for (int i=0; i<nquad; ++i)
+    {
+        int j = i + k * nquad;
+        T[j] = TL + i * inc;
+        P[j] = PC;
+        V[j] = m * R * T[j] / P[j];
+
+        work += dw(j);
+    }
+}
+
+void isothermal_heat_rejection(int k, double Vmin, double Vmax)
+{
+    double inc = (Vmax - Vmin) / (nquad+1);
+    for (int i=0; i<nquad; ++i)
+    {
+        int j = i + k * nquad;
+        //V[j] = VH - i * inc;
+        V[j] = Vmax - i * inc;
         T[j] = TL;
         P[j] = m * R * T[j] / V[j];
 
@@ -93,13 +133,14 @@ void isentropic_expansion(int k)
     }
 }
 
-void isothermal_heat_addition(int k, double VI)
+void isothermal_heat_addition(int k, double Vmin, double Vmax)
 {
-    double inc = (VI - VL) / (nquad);
+    double inc = (Vmax - Vmin) / (nquad);
     for (int i=0; i<nquad; ++i)
     {
         int j = i + k * nquad;
-        V[j] = VL + i * inc;
+        //V[j] = VL + i * inc;
+        V[j] = Vmin + i * inc;
         T[j] = TH;
         P[j] = m * R * T[j] / V[j];
 
@@ -143,9 +184,9 @@ void stirling()
 {
     work = 0.;
 
-    isothermal_heat_rejection(0, VL);
+    isothermal_heat_rejection(0, VL, VH);
     isochoric_compression(1);
-    isothermal_heat_addition(2, VH);
+    isothermal_heat_addition(2, VL, VH);
     isochoric_expansion(3);
 
     print("stirling");
@@ -159,12 +200,29 @@ void carnot()
 
     isentropic_compression(0);
     isentropic_expansion(2);
-    isothermal_heat_addition(1, V[nquad*2]);
-    isothermal_heat_rejection(3, V[0]);
+    isothermal_heat_addition(1, VL, V[nquad*2-1]);
+    isothermal_heat_rejection(3, V[0], VH);
 
     print("carnot");
 
     std::cout << "net work from carnot: " << work << std::endl;
+}
+
+void ericsson()
+{
+    work = 0.;
+
+    double PL = m * R * TH / VH; // 4
+    double PH = m * R * TL / VL; // 2
+
+    isobaric_heat_rejection(0, PL);
+    isothermal_heat_rejection(1, VL, V[nquad*1-1]);
+    isobaric_heat_addition(2, PH);
+    isothermal_heat_addition(3, V[nquad*3-1], VH);
+
+    print("ericsson");
+
+    std::cout << "net work from ericsson: " << work << std::endl;
 }
 
 
@@ -172,6 +230,17 @@ int main()
 {
     stirling();
     carnot();
+    ericsson();
+
+    //https://www.quora.com/Why-do-we-add-heat-at-a-constant-pressure-in-a-gas-turbine-plant
+    // https://en.wikipedia.org/wiki/Brayton_cycle
+    // https://dieselnet.com/tech/diesel_engines.php
+    // https://engineering.stackexchange.com/questions/32204/why-does-combustion-happen-at-constant-pressure-in-jet-engines
+    // https://en.wikipedia.org/wiki/Pulsejet
+    // Ramjet
+    // mixed pdv and vdp
+    // https://www.physicsforums.com/threads/why-does-heat-addition-happen-at-constant-pressure-in-diesel-cycle.746745/
+    // https://web.mit.edu/16.unified/www/FALL/thermodynamics/notes/node40.html
         
 
     return 0;
--- plot.gp
+++ plot.gp
@@ -1,7 +1,6 @@
 set xrange[0:0.7];
 set xlabel "V"
 set ylabel "P"
-plot "carnot" using 3:2 with lines lw 3 title "Carnot", "stirling" using 3:2 with lines lw 5 title "Stirling"
-
+plot "carnot" using 3:2 with lines lw 3 title "Carnot", "stirling" using 3:2 with lines lw 3 title "Stirling", "ericsson" using 3:2 with lines lw 3 title "Ericsson"
 
 pause -1
