dad93b769df3cbcba1f3a7cf0cd07535639ef8de
orxshi <orxan.shibli@gmail.com>
2023-04-04 23:30:13 +0300

    >

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <cmath>
 #include <string>
+#include <cassert>
 
 //const int n = 120;
 const int n = 4320;
@@ -31,15 +32,15 @@ double dw(int j)
     return P[j] * (V[j] - V[j-1]);
 }
 
-double dq(int j)
-{
-    if (j == 0)
-    {
-        return 0.;
-    }
-
-    return T[j] * (S[j] - S[j-1]);
-}
+//double dq(int j)
+//{
+//    if (j == 0)
+//    {
+//        return 0.;
+//    }
+//
+//    return T[j] * (S[j] - S[j-1]);
+//}
 
 double work = 0.;
 
@@ -54,7 +55,6 @@ void isobaric_heat_rejection(int k, double PC)
         V[j] = m * R * T[j] / P[j];
 
         work += dw(j);
-        heat += 
     }
 }
 
@@ -101,10 +101,60 @@ void isochoric_compression(int k)
     }
 }
 
+void isentropic_full_compression(int k)
+{
+    double incv = (VH - VL) / (nquad);
+    double TI = TL * std::pow(VH/VL,(GM-1));
+    double inct = (TI - TL) / (nquad);
+
+    for (int i=0; i<nquad; ++i)
+    {
+        int j = i + k * nquad;
+        V[j] = VH - i * incv;
+        T[j] = TL + i * inct;
+        P[j] = m * R * T[j] / V[j];
+
+        work += dw(j);
+    }
+}
+
+void isentropic_full_expansion(int k)
+{
+    double incv = (VH - VL) / (nquad);
+    double TI = TH * std::pow(VH/VL,(GM-1));
+    double inct = (TH - TI) / (nquad);
+
+    for (int i=0; i<nquad; ++i)
+    {
+        int j = i + k * nquad;
+        V[j] = VL + i * incv;
+        T[j] = TH - i * inct;
+        P[j] = m * R * T[j] / V[j];
+
+        work += dw(j);
+    }
+}
+
 void isentropic_compression(int k)
 {
     double inct = (TH - TL) / (nquad);
     double VI = VL * std::pow(TH/TL,1/(GM-1));
+    double incv = (VI - VL) / (nquad);
+    for (int i=0; i<nquad; ++i)
+    {
+        int j = i + k * nquad;
+        V[j] = VI - i * incv;
+        T[j] = TL + i * inct;
+        P[j] = m * R * T[j] / V[j];
+
+        work += dw(j);
+    }
+}
+
+void isentropic_compression(int k, double TI)
+{
+    double inct = (TI - TL) / (nquad);
+    double VI = VL * std::pow(TI/TL,1/(GM-1));
     double incv = (VI - VL) / (nquad);
     for (int i=0; i<nquad; ++i)
     {
@@ -122,11 +172,46 @@ void isentropic_expansion(int k)
     double inct = (TH - TL) / (nquad);
     double VI = VH * std::pow(TL/TH,1/(GM-1));
     double incv = (VH - VI) / (nquad);
+    assert(VI < VH);
     for (int i=0; i<nquad; ++i)
     {
         int j = i + k * nquad;
         V[j] = VI + i * incv;
         T[j] = TH - i * inct;
+        P[j] = m * R * T[j] / V[j];
+
+        work += dw(j);
+    }
+}
+
+void isentropic_expansion(int k, double TI)
+{
+    double inct = (TI - TL) / (nquad);
+    double VI = VH * std::pow(TL/TH,1/(GM-1));
+    double incv = (VH - VI) / (nquad);
+    assert(VI < VH);
+    for (int i=0; i<nquad; ++i)
+    {
+        int j = i + k * nquad;
+        V[j] = VI + i * incv;
+        T[j] = TI - i * inct;
+        P[j] = m * R * T[j] / V[j];
+
+        work += dw(j);
+    }
+}
+
+void isothermal_heat_addition_Q(int k, double Vmin, double QH)
+{
+    double VI = VL * std::exp(QH/m/R/TH); 
+    double inc = (VI - Vmin) / (nquad);
+    assert(VI > Vmin);
+
+    for (int i=0; i<nquad; ++i)
+    {
+        int j = i + k * nquad;
+        V[j] = Vmin + i * inc;
+        T[j] = TH;
         P[j] = m * R * T[j] / V[j];
 
         work += dw(j);
@@ -156,6 +241,45 @@ void isochoric_expansion(int k)
         int j = i + k * nquad;
         V[j] = VH;
         T[j] = TH - i * inc;
+        P[j] = m * R * T[j] / V[j];
+    }
+}
+
+void isochoric_heat_addition(int k, double TI, double QH)
+{
+    double TH_real = TI + QH / m / cp; 
+    double inc = (TH_real - TI) / (nquad);
+    assert(inc > 0);
+    for (int i=0; i<nquad; ++i)
+    {
+        int j = i + k * nquad;
+        V[j] = VL;
+        T[j] = TI + i * inc;
+        P[j] = m * R * T[j] / V[j];
+    }
+}
+
+void isochoric_heat_addition(int k, double TI)
+{
+    double inc = (TH - TI) / (nquad);
+    assert(inc > 0);
+    for (int i=0; i<nquad; ++i)
+    {
+        int j = i + k * nquad;
+        V[j] = VL;
+        T[j] = TI + i * inc;
+        P[j] = m * R * T[j] / V[j];
+    }
+}
+
+void isochoric_heat_rejection(int k, double TI)
+{
+    double inc = (TI - TL) / (nquad);
+    for (int i=0; i<nquad; ++i)
+    {
+        int j = i + k * nquad;
+        V[j] = VH;
+        T[j] = TI - i * inc;
         P[j] = m * R * T[j] / V[j];
     }
 }
@@ -194,13 +318,28 @@ void stirling()
     std::cout << "net work from stirling: " << work << std::endl;
 }
 
-void carnot()
+void carnot1()
 {
     work = 0.;
 
     isentropic_compression(0);
     isentropic_expansion(2);
-    isothermal_heat_addition(1, VL, V[nquad*2-1]);
+    isothermal_heat_addition(1, VL, V[nquad*2]);
+    isothermal_heat_rejection(3, V[0], VH);
+
+    print("carnot");
+
+    std::cout << "net work from carnot: " << work << std::endl;
+}
+
+void carnot2()
+{
+    // does not seem possible.
+    work = 0.;
+
+    isothermal_heat_addition_Q(1, VL, 100);
+    isentropic_expansion(2, T[2*nquad-1]);
+    isentropic_compression(0, T[2*nquad-1]);
     isothermal_heat_rejection(3, V[0], VH);
 
     print("carnot");
@@ -225,12 +364,28 @@ void ericsson()
     std::cout << "net work from ericsson: " << work << std::endl;
 }
 
+void otto()
+{
+    // setting compression ratio and temperature range causes contradiction sometimes. If suitable parameters are found, in this case, Carnot fails. Failure happens this way: Max volume may be exceeded or max temperature may be exceeded.
+    work = 0.;
+
+    isentropic_full_compression(0);
+    isochoric_heat_addition(1, T[nquad-1], 500);
+    isentropic_full_expansion(2);
+    isochoric_heat_rejection(3, T[3*nquad-1]);
+
+    print("otto");
+
+    std::cout << "net work from otto: " << work << std::endl;
+}
+
 
 int main()
 {
     stirling();
-    carnot();
+    carnot1();
     ericsson();
+    otto(); // plot otto. you will see that it gives wrong plot.
 
     //https://www.quora.com/Why-do-we-add-heat-at-a-constant-pressure-in-a-gas-turbine-plant
     // https://en.wikipedia.org/wiki/Brayton_cycle
--- plot.gp
+++ plot.gp
@@ -1,6 +1,8 @@
-set xrange[0:0.7];
+set xrange[0:0.9];
 set xlabel "V"
 set ylabel "P"
-plot "carnot" using 3:2 with lines lw 3 title "Carnot", "stirling" using 3:2 with lines lw 3 title "Stirling", "ericsson" using 3:2 with lines lw 3 title "Ericsson"
+#plot "carnot" using 3:2 with lines lw 3 title "Carnot", "stirling" using 3:2 with lines lw 3 title "Stirling", "ericsson" using 3:2 with lines lw 3 title "Ericsson"
+plot "otto" using 3:2 with lines lw 3 title "Otto"
+#plot "carnot" using 3:2 with lines lw 1 title "Carnot"
 
 pause -1
